46425482b71b61f94113a4894dc28c3743ff656b
Carl Vancluysen <Carl.Vancluysen@gmail.com>
2018-02-28 14:27:36 +0100

    Settings fixed, record page and listview added

---
+++ Android/app/src/main/java/be/eaict/sleepqualitymeter/records.java
@@ -0,0 +1,81 @@
+package be.eaict.sleepqualitymeter;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * Created by CarlV on 2/28/2018.
+ */
+
+public class records extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_records);
+        ListView listView = findViewById(R.id.recListv);
+
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+    }
+        class CustomAdapter extends BaseAdapter {
+            @Override
+            public int getCount() {
+                return 5;
+            }
+
+            @Override
+            public Object getItem(int i) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return 0;
+            }
+
+            @Override
+            public View getView(final int i, View view, ViewGroup viewGroup) {
+                final int current = i;
+                view = getLayoutInflater().inflate(R.layout.listview_records, null);
+                TextView listDate = view.findViewById(R.id.recListDate);
+                TextView listSleepTime = view.findViewById(R.id.recListTime);
+                TextView listSummary = view.findViewById(R.id.recListSummary);
+                listDate.setText("12/01/1993");
+                listSleepTime.setText("8:21");
+                listSummary.setText("Good!");
+                listDate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        intentDetail();
+                    }
+                });
+                listSleepTime.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+                listSummary.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+                //    listSummary.setBackgroundColor();
+                return view;
+            }
+        }
+        public void intentDetail() {
+
+
+
+        }
+
+    }
