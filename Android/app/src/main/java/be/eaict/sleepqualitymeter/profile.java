75c7f8859553bf8781dd1b45c5187a03fc7e9fec
JohnBetohn <34214552+JohnBetohn@users.noreply.github.com>
2018-03-01 15:32:50 +0100

    Too quick to commit. Actually added dock to profile

--- Android/app/src/main/java/be/eaict/sleepqualitymeter/profile.java
+++ Android/app/src/main/java/be/eaict/sleepqualitymeter/profile.java
@@ -3,6 +3,7 @@ package be.eaict.sleepqualitymeter;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +31,11 @@ public class profile extends AppCompatActivity {
         txtWeight = findViewById(R.id.profTxtWeight);
         txtAvgSleepTime = findViewById(R.id.profTxtAvgSleepTime);
         txtAvgSleepTime.setText("N/A");
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_profile);
+        DockNavigation dockNavigation = new DockNavigation(bottomNavigationView, getBaseContext());
+
         Button btnSettings = findViewById(R.id.profBtnSettings);
         btnSettings.setOnClickListener(new View.OnClickListener() {
             @Override
