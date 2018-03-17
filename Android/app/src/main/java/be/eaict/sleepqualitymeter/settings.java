c32871ce6107c94677510b0b2dac86743699bd3c
Carl Vancluysen <Carl.Vancluysen@gmail.com>
2018-03-17 20:13:49 +0100

    Settings done

--- Android/app/src/main/java/be/eaict/sleepqualitymeter/settings.java
+++ Android/app/src/main/java/be/eaict/sleepqualitymeter/settings.java
@@ -17,6 +17,12 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.mukesh.countrypicker.Country;
 import com.mukesh.countrypicker.CountryPicker;
 import com.mukesh.countrypicker.CountryPickerListener;
@@ -26,7 +32,7 @@ import com.mukesh.countrypicker.CountryPickerListener;
  */
 
 public class settings extends AppCompatActivity {
-    String firstName, lastName, password, newcountry, oldcountry, weight;
+    String firstName, lastName, password, newcountry, oldcountry, weight, email, userID;
     Boolean switchTemperature , switchLight, switchMeasurement;
     Switch temperature, light, measurement;
     EditText editFirstName, editLastName, editPassword, editWeight;
@@ -35,7 +41,10 @@ public class settings extends AppCompatActivity {
     //Firebase
     FirebaseAuth mAuth;
     FirebaseUser firebaseUser;
+    DatabaseReference databaseReference;
     ImageView imgCountry;
+    User user;
+    DatabaseReference dbFirstName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,12 +52,11 @@ public class settings extends AppCompatActivity {
         switchTemperature = false;
         switchLight = false;
         switchMeasurement = false;
-
-        Load();
-
         mAuth = FirebaseAuth.getInstance();
         firebaseUser = mAuth.getCurrentUser();
-
+        databaseReference = FirebaseDatabase.getInstance().getReference("User");
+        dbFirstName = FirebaseDatabase.getInstance().getReference().child("User").child("firstname");
+        Load();
         selectCountry = findViewById(R.id.setTxtSelectCountry);
         Button savebutton = findViewById(R.id.setBtnSave);
         Button saveDiscard = findViewById(R.id.setBtnDiscard);
@@ -58,11 +66,13 @@ public class settings extends AppCompatActivity {
         editFirstName = findViewById(R.id.setEditFirstName);
         editLastName = findViewById(R.id.setEditLastName);
         editPassword = findViewById(R.id.setEditPassw);
+        editWeight = findViewById(R.id.setEditWeight);
         imgCountry = findViewById(R.id.setImgCountry);
         editWeight.setText(weight);
         selectCountry.setText(oldcountry);
-        Country tempcountry = Country.getCountryByName(oldcountry);
-        imgCountry.setImageResource(tempcountry.getFlag());
+        editFirstName.setText(firstName);
+    //    Country tempcountry = Country.getCountryByName(oldcountry);
+    //    imgCountry.setImageResource(tempcountry.getFlag());
         savebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -121,56 +131,102 @@ public class settings extends AppCompatActivity {
     }
 
     public void Save() {
-        String newpassword = editPassword.getText().toString();
-        String newfirstName = editFirstName.getText().toString();
-        String newlastName = editLastName.getText().toString();
+        final String newpassword = editPassword.getText().toString();
+        final String newfirstName = editFirstName.getText().toString();
+        final String newlastName = editLastName.getText().toString();
         String newweight = editWeight.getText().toString();
         SharedPreferences sp = getSharedPreferences("DATA", MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         editor.putBoolean("light", switchLight);
         editor.putBoolean("measurement", switchMeasurement);
         editor.putBoolean("temp", switchTemperature);
-
         //Set password Firebase
-        if(!newpassword.isEmpty() && newpassword != password){
-            if(password.length() < 6){
-                editPassword.setError("Minimum lenght of password should be 6");
-                editPassword.requestFocus();
-                return;
-            }
-            firebaseUser.updatePassword(password);
-        }
-        if(newfirstName != firstName) {
-            //zet newfirstName als nieuwe firstName in DB
-        }
-        if(newlastName != lastName) {
-            //Zet newlastName als lastName in DB
-        }
-        if(newweight != weight) {
-            if(Integer.getInteger(newweight) < 200) {
+                    if(!newpassword.isEmpty()){
+                        if(password.length() < 6){
+                            editPassword.setError("Minimum lenght of password should be 6");
+                            editPassword.requestFocus();
+                            return;
+                        }
+                        firebaseUser.updatePassword(newpassword);
+                    }
+                    if(newfirstName != firstName && !editFirstName.toString().isEmpty()) {
+                        databaseReference.child(userID).child("firstname").setValue(newfirstName);
+                    }
+                    if(newlastName != lastName && !editLastName.toString().isEmpty()) {
+                        databaseReference.child(userID).child("lastname").setValue(newlastName);
+                    }
+        if(newweight != weight && !editWeight.toString().isEmpty()) {
+         //   int t = Integer.getInteger(editWeight.getText().toString().);
+          /*  if(t  > 200) {
                 editWeight.setError("Max weight is 200");
                 editWeight.requestFocus();
                 return;
-            }
+            } */
             if (measurement.isChecked() == false) {
-                //Zet newweight als weight in DB
+                databaseReference.child(userID).child("weight").setValue(newweight);
             }
             else {
                 Double tempweight = Double.parseDouble(newweight) * 0.45359237;
-                int Finalweight = tempweight.intValue();
-                //Zet Finalweight als weight in DB
+                int tempt = tempweight.intValue();
+                databaseReference.child(userID).child("weight").setValue(Integer.toString(tempt));
+
             }
         }
-        if (newcountry != oldcountry) {
-            //Zet newcountry als country in DB
+        if (newcountry != oldcountry && newcountry != null) {
+            databaseReference.child(userID).child("country").setValue(newcountry);
         }
         editor.apply();
         Toast.makeText(this, "Saved", Toast.LENGTH_SHORT);
     }
+
     public void Load() {
         SharedPreferences sp = getSharedPreferences("DATA", MODE_PRIVATE);
         switchMeasurement  = sp.getBoolean("measurement", false);
         switchLight = sp.getBoolean("light", false);
         switchTemperature = sp.getBoolean("temp", false);
+        email = mAuth.getCurrentUser().getEmail().toLowerCase();
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String rawdata_weight = null;
+
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    user = snapshot.getValue(User.class);
+                    System.out.println(user.getEmail());
+
+                    if(user.getEmail().equals(email)){
+                        System.out.println("WOOOOHOOO");
+                        userID = user.getId();
+                        firstName = user.getFirstname();
+                        lastName = user.getLastname();
+                        oldcountry = user.getCountry();
+                        rawdata_weight = user.getWeight();
+                        selectCountry.setText(oldcountry);
+                        editFirstName.setText(firstName);
+                        editLastName.setText(lastName);
+                    }
+                }
+
+                //Convert KG to pound
+                if(measurement.isChecked() ==true) {
+                    Double lbstokg = Double.parseDouble(rawdata_weight) / 0.45359237;
+                    int t = lbstokg.intValue();
+                    weight = Integer.toString(t);
+                    editWeight.setText(weight);
+                }
+                else {
+                    weight = rawdata_weight;
+                    editWeight.setText(weight);
+                }
+
+                Country tempcountry = Country.getCountryByName(oldcountry);
+                imgCountry.setImageResource(tempcountry.getFlag());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                throw databaseError.toException();
+            }
+        });
     }
     }
