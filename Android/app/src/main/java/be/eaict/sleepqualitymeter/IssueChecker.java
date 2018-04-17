4639392ae0bc69bdaf282bd5b7fef47eab1689a4
Savodir <Carl.Vancluysen@gmail.com>
2018-04-18 00:09:13 +0200

    IssueChecker Lux/Hum/Noise/Temp

--- Android/app/src/main/java/be/eaict/sleepqualitymeter/IssueChecker.java
+++ Android/app/src/main/java/be/eaict/sleepqualitymeter/IssueChecker.java
@@ -1,8 +1,16 @@
 package be.eaict.sleepqualitymeter;
 
+import android.graphics.Color;
+import android.media.MediaExtractor;
+import android.util.Log;
+
+import java.nio.channels.FileLock;
+import java.util.ArrayList;
+import java.util.List;
+
 public class IssueChecker {
     DataRepo repo;
-    String sleepQuality, heartRateQuality;
+    String sleepQuality, heartRateQuality, luxQuality, tempQuality, noiseQuality, humidityQuality;
     int issuecounter;
     LogicandCalc logicandCalc;
     public IssueChecker(DataRepo repo) {
@@ -27,13 +35,106 @@ public class IssueChecker {
         }
     }
     public void heartRate() {
+        List<Float> luxvalues = new ArrayList<>();
+        List<Float> humidityvalues = new ArrayList<>();
+        List<Float> noisevalues = new ArrayList<>();
+        List<Float> tempvalues = new ArrayList<>();
+
+        float luxvalue = 0, humidityvalue = 0, noisevalue = 0, tempvalue = 0;
+        Boolean heartRateHit = false;
+        Boolean temperatureHit = false;
+        Boolean humidityHit = false;
+        Boolean noiseHit = false;
+        Boolean luxHit = false;
+        int timer = 0;
         for(int i = 0; i < repo.Repo.size(); i++) {
-            if(repo.Repo.get(i).getHeartbeat() > 110) {
+            if(repo.Repo.get(i).getHeartbeat() > 110 && heartRateHit == false) {
                 issuecounter++;
-                heartRateQuality = "Heartbeat went too high!";
+                heartRateHit = true;
+                heartRateQuality = "Heartbeat went over 110!";
             }
             else heartRateQuality = "Good";
+            luxvalue = luxvalue + repo.Repo.get(i).getLuminosity();
+            humidityvalue =  humidityvalue + repo.Repo.get(i).getHumidity();
+            noisevalue = noisevalue + repo.Repo.get(i).getNoise();
+            tempvalue = tempvalue + repo.Repo.get(i).getTemperature();
+            timer++;
+            if(timer == 3) {
+                luxvalues.add(luxvalue / 3);
+                humidityvalues.add(humidityvalue / 3);
+                noisevalues.add(noisevalue / 3);
+                tempvalues.add(tempvalue / 3);
+                luxvalue = 0;
+                humidityvalue = 0;
+                noisevalue = 0;
+                tempvalue = 0;
+                timer = 0;
+            }
         }
+        for(int i = 0; i <= luxvalues.size() - 1; i++) {
+            Log.d("CheckerTemp", Float.toString(tempvalues.get(i)));
+            if(luxvalues.get(i) >= 6 && luxHit == false) {
+                luxHit = true;
+                issuecounter++;
+                luxQuality = "Lux went over 6!";
+                Log.d("Checker", luxQuality);
+            }
+            if(noisevalues.get(i) >= 40 && noiseHit == false) {
+                noiseHit = true;
+                issuecounter++;
+                noiseQuality = "Decibels went over 40";
+                Log.d("Checker", noiseQuality);
+
+            }
+            if(tempvalues.get(i) >= 19 && temperatureHit == false) {
+                temperatureHit = true;
+                issuecounter++;
+                tempQuality = "Room temperature went over 18.5 degrees celcius";
+                Log.d("Checker", tempQuality);
+
+            }
+            if(humidityvalues.get(i) >= 60 && humidityHit == false) {
+                humidityHit = true;
+                issuecounter++;
+                humidityQuality = "Humidty went over 60";
+                Log.d("Checker", humidityQuality);
+            }
+        }
+        Log.d("Luxlength", Integer.toString(luxvalues.size()));
+        for(int i = 0; i <= luxvalues.size() - 1; i++) {
+            Log.d("Luxvalues", Float.toString(luxvalues.get(i)));
+        }
+
+    }
+    public int ColorPicker() {
+        int color = 0;
+        if (issuecounter != 0) {
+            switch (issuecounter) {
+                case 0:
+                    color = Color.rgb(0, 255, 0);
+                break;
+                case 1:
+                    color = Color.rgb(42, 213, 0);
+                break;
+                case 2:
+                    color = Color.rgb(84, 171, 0);
+                break;
+                case 3:
+                    color = Color.rgb(126, 129, 0);
+                break;
+                case 4:
+                    color = Color.rgb(170, 87, 0);
+                break;
+                case 5:
+                    color = Color.rgb(214, 45, 0);
+                break;
+                case 6:
+                    color = Color.rgb(254, 3, 0);
+                break;
+            }
+        }
+        else color = Color.rgb(0,0,0);
+        return color;
     }
 }
 
